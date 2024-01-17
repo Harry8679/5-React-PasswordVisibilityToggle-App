dab8c330c6f08714308861ec6fec77880983dac6
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-17 10:42:30 +0100

    Password Visibility Toggle - Logic

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
 import { IoIosEyeOff, IoIosEye } from "react-icons/io";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  }
+
   return (
     <section className='login --100vh --center-all'>
         <div className="--card --bg-light">
@@ -10,9 +16,13 @@ const Login = () => {
             <div className="--form-control mySpace">
                 <input type="email" placeholder='Email' className='--width-100' />
                 <div className="password">
-                    <input type="password" placeholder='Password' className='--width-100' />
-                    <div className="icon">
-                        <IoIosEye />
+                    <input type={!showPassword ? 'password' : 'text'} placeholder='Password' className='--width-100' />
+                    <div className="icon" onClick={handleTogglePassword}>
+                        {!showPassword ? (
+                            <IoIosEye />
+                        ): (
+                            <IoIosEyeOff />
+                        )}
                     </div>
                 </div>
                 <button className='--btn --btn-success --btn-block'>Login</button>
